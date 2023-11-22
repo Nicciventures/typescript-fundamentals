6afdc3df85f94e2d380943ec0e39765b43de144d
Nicci <139131207+NicciThelen@users.noreply.github.com>
2023-11-22 14:12:14 +0100

    course continuation - added async functions

--- src/4-Creating-And-Using-Functions/output.ts
+++ src/4-Creating-And-Using-Functions/output.ts
@@ -1,8 +1,50 @@
+import { productsURL } from "../lib";
+
 const prefix = '🐉 ';
 
+type ProductType = {
+  id: number;
+  name: string;
+  icon?: string
+}
+
 export default async function updateOutput(id: string) {
+  const products = await getProducts();
+  const output = document.querySelector(`#${id}`);
  // TO DO 
 }
+
+function layoutProducts(products: ProductType[]) {
+  const items = products.map(({ id, name, icon }) => {
+    const productHtml = `
+    <span class="card-id">#${id}</span>
+      <i class="card-icon ${icon} fa-lg"></i>
+    <span class="card-name">${name}</span>
+    `;
+    const cardHtml = `
+    <li>
+        <div class="card">
+            <div class="card-content">
+                <div class="content">
+                ${productHtml}
+                </div>
+            </div>
+        </div>
+    </li>
+    `;
+    return cardHtml;
+  });
+  let productsHtml = `<ul>${items.join('')}</ul>`;
+  return productsHtml;
+}
+
+
+async function getProducts(): Promise<ProductType[]> {
+  const response: Response = await fetch(productsURL);
+  const products: ProductType[] = await response.json();
+  return products;
+}
+
 // run our samples
 runTheLearningSamples();
 
@@ -18,7 +60,7 @@ function runTheLearningSamples() {
   console.log(`${prefix} function declaration`)
   console.log(addNumbersDeclaration(7, 11));
   
-  function addNumbersDeclaration(x: number, y: number) {
+  function addNumbersDeclaration(x: number, y: number): number {
     const sum = x + y;
     return sum;
   }
@@ -31,4 +73,48 @@ function runTheLearningSamples() {
   console.log(`${prefix} function declaration`)
   console.log(addNumbersExpression(77, 111));
 
+  const sampleProducts = [
+    {
+      id: 10,
+      name: 'Pizza slice',
+      icon: 'fas fa-pizza-slice',
+    },
+    {
+      id: 20,
+      name: 'Ice Cream',
+      icon: 'fas fa-ice-cream',
+    },
+    {
+      id: 30,
+      name: 'Cheese',
+      icon: 'fas fa-cheese'
+    }
+  ]
+
+  function getProductNames(): string[] {
+    return sampleProducts.map((p) => p.name)
+  }
+
+  console.log(`${prefix} return array`);
+  console.log(getProductNames());
+
+  function getProductById(id: number): ProductType | undefined {
+    return sampleProducts.find(p => id === p.id)
+
+  }
+
+  console.log(`${prefix} return ProductType`);
+  console.log(getProductById(50));
+
+  function displayProducts(products: ProductType[]): void {
+    const productNames = products.map(p => {
+      const name = p.name.toLowerCase();
+      return name;
+    });
+    const msg = `Sample products include: ${productNames.join(', ')}`;
+    console.log(`${prefix} return void`);
+    console.log(msg);
+  }
+
+  displayProducts(sampleProducts);
 }
