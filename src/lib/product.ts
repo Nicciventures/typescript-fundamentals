9117ffdb47bb92454f31187c5fedf2485fe8800d
Nicci <139131207+NicciThelen@users.noreply.github.com>
2023-11-29 11:39:09 +0100

    classes, extend, implement, abstract

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,15 +1,43 @@
-import { Product } from './interfaces';
+import { Product } from "./interfaces";
 
-// This will act as the foundation for other Product type classes (FoodProduct, SportingProduct)
+// abstract indicates building block class / makes a foundation. can't new it up
+// implement ensure that the class does as the 'Product' says you should
 abstract class ProductBase implements Product {
-  constructor(public id: number, public name: string, public icon: string) {}
+  // constructor (auto implemented properties)
+  constructor(public id: number, public name: string, public icon: string) { }
+
   validate(): boolean {
     throw new Error('Not implemented');
   }
 }
 
 export class FoodProduct extends ProductBase {
-  validate(): boolean {
+  // functions
+  validate() : boolean {
     return !!this.id && !!this.name && !!this.icon;
   }
 }
+
+class SportingGoodProduct extends ProductBase {
+  constructor (id: number, name: string, icon: string) {
+    super(id, name, icon)
+  }
+}
+
+// BELOW IS GITHUB CODE ALREADY PROVIDE. ABOVE IS IMPLEMENTED DURING THE EXPLANATION
+
+// import { Product } from './interfaces';
+
+// // This will act as the foundation for other Product type classes (FoodProduct, SportingProduct)
+// abstract class ProductBase implements Product {
+//   constructor(public id: number, public name: string, public icon: string) {}
+//   validate(): boolean {
+//     throw new Error('Not implemented');
+//   }
+// }
+
+// export class FoodProduct extends ProductBase {
+//   validate(): boolean {
+//     return !!this.id && !!this.name && !!this.icon;
+//   }
+// }
